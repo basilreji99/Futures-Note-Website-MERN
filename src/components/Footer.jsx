10566f90f3be376e7e26845507deb549d2c95711
Basil Reji <basil@Basil_Reji>
2024-03-23 21:01:54 -0400

    Added html, css and JSX files. JSX files include App, Header, Footer and Notes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer(){
+    const currentYear = new Date().getFullYear();
+    <footer>
+    return <p>copyright © {currentYear}</p>;
+    </footer>
+}
+
+export default Footer;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Notes(){
+    return (
+        <div className="note">
+            <h1> Heading</h1>
+            <p> Note</p>
+        </div>
+    );
+}
+ export default Notes;
